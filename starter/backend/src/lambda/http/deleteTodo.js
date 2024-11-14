68c77d8d3946196750e7bbefa0fde63757c9b301
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-11-14 13:50:24 +0100

    implement delete todo

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,25 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import {getUserId} from '../utils.mjs'
+import {deleteTodo} from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
+const deleteTodoHandler = async (event) => {
+    const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
+    await deleteTodo(userId, todoId)
 
-  // TODO: Remove a TODO item by id
-  return undefined
+    return {
+        statusCode: 204,
+        body: JSON.stringify({
+            message: "Delete successfully."
+        })
+    }
 }
+
+export const handler = middy(deleteTodoHandler)
+    .use(httpErrorHandler())
+    .use(cors({
+        credentials: true
+    }))
 
