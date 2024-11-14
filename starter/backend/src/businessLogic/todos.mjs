7590b048c2806cd5b8ebc8fd52eb7d9a8b12ead9
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-11-14 13:30:10 +0100

    implement business logic and get todo lambda evnt

---
+++ starter/backend/src/businessLogic/todos.mjs
@@ -0,0 +1,47 @@
+import * as uuid from 'uuid'
+import {createLogger} from '../utils/logger.mjs'
+import {todosAccess} from '../dataLayer/todosAccess.mjs'
+
+const logger = createLogger('CRUD Business Logic')
+const bucketName = process.env.BUCKET_NAME
+
+// CREATE
+export async function createTodo(userId, body) {
+    logger.info('Create Processing.')
+    const todoId = uuid.v4()
+    const createdAt = new Date().toISOString()
+    const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
+    const todo = {
+        userId,
+        todoId,
+        createdAt,
+        done: false,
+        attachmentUrl: attachmentUrl,
+        ...body
+    }
+    return await todosAccess.createTodo(todo)
+}
+
+// REFER
+export async function getTodos(userId) {
+    logger.info('Get all processing.')
+    return await todosAccess.getTodos(userId)
+}
+
+// UPDATE
+export async function updateTodo(userId, todoId, body) {
+    logger.info('Update processing.')
+    return await todosAccess.updateTodo(userId, todoId, body)
+}
+
+// DELETE
+export async function deleteTodo(userId, todoId) {
+    logger.info('Delete processing.')
+    return await todosAccess.deleteTodo(userId, todoId)
+}
+
+// UPLOAD IMAGE
+export async function generateUploadUrl(todoId) {
+    logger.info('Upload processing.')
+    return await todosAccess.getUploadUrl(todoId)
+}
--- starter/backend/src/lambda/http/getTodos.js
+++ starter/backend/src/lambda/http/getTodos.js
@@ -1,4 +1,23 @@
-export function handler(event) {
-  // TODO: Get all TODO items for a current user
-  return undefined
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import {getUserId} from '../utils.mjs'
+import {getTodos} from '../../businessLogic/todos.mjs'
+
+const getTodosHandler = async (event) => {
+    const userId = getUserId(event)
+    const todos = await getTodos(userId)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            items: todos
+        })
+    }
 }
+
+export const handler = middy(getTodosHandler)
+    .use(httpErrorHandler())
+    .use(cors({
+        credentials: true
+    }))
