b8b6f91ce0ad22825fcbcf65ac3f8f158d200a5c
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-11-14 13:36:10 +0100

    add create todo lambda handler

--- starter/backend/src/lambda/http/createTodo.js
+++ starter/backend/src/lambda/http/createTodo.js
@@ -1,8 +1,24 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import {getUserId} from '../utils.mjs'
+import {createTodo} from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const newTodo = JSON.parse(event.body)
+const createTodoHandler = async (event) => {
+  const userId = getUserId(event)
+  const body = JSON.parse(event.body)
+  const todo = await createTodo(userId, body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      item: todo
+    })
+  }
 }
 
+export const handler = middy(createTodoHandler)
+    .use(httpErrorHandler())
+    .use(cors({
+      credentials: true
+    }))
