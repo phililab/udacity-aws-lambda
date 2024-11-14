d53fe3b9ffbe6db3700e2bff826953222c3e41c0
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-11-14 14:22:16 +0100

    implemend http handler

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,23 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import {generateUploadUrl} from '../../businessLogic/todos.mjs'
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+const generateUploadUrlHandler = async (event) => {
+  const todoId = event.pathParameters.todoId
+  const url = await generateUploadUrl(todoId)
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
 }
+
+export const handler = middy(generateUploadUrlHandler)
+    .use(httpErrorHandler())
+    .use(cors({
+      credentials: true
+    }))
 
