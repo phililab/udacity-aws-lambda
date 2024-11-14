b093cf3686329803bd3c29761f6171ddd3362844
Philippe Labonde <philippe.labonde@arvato-scs.com>
2024-11-14 13:42:34 +0100

    implement update todo handler

--- starter/backend/src/lambda/http/updateTodo.js
+++ starter/backend/src/lambda/http/updateTodo.js
@@ -1,7 +1,26 @@
-export function handler(event) {
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import {getUserId} from '../utils.mjs'
+import {updateTodo} from '../../businessLogic/todos.mjs'
+
+const updateTodoHandler = async (event) => {
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
-  const updatedTodo = JSON.parse(event.body)
-  
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const body = JSON.parse(event.body)
+
+  await updateTodo(userId, todoId, body)
+
+  return {
+    statusCode: 204,
+    body: JSON.stringify({
+      message: "Updated successfully."
+    })
+  }
 }
+
+export const handler = middy(updateTodoHandler)
+    .use(httpErrorHandler())
+    .use(cors({
+      credentials: true
+    }))
